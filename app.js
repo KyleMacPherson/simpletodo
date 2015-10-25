781081bb4f4047ef773d500c5e12d6c00d3b217f
kyle macpherson <kaimac2010@gmail.com>
2015-10-25 21:54:09 +0000

    todoctrl added

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-var app = angular.module('simpleToDo', []);
+(function() {
+
+  var app = angular.module('simpleToDo', []);
+
+  app.controller('ToDoController', function() {
+    this.addTask = function (task) {
+      var self = this;
+      this.list = [];
+      var newTask = {
+        name: task,
+        completed: false,
+        showing: true
+      }
+      self.list.push(newTask)
+      self.task = '';
+    }
+  });
+
+})();
